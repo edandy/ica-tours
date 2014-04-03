64fca8063aa9b8989ee0a0b681a4f10b945be791
edandy <edandy.perez@gmail.com>
2014-04-03 00:09:19 -0500

    archi

---
+++ js/app.js
@@ -0,0 +1,9 @@
+$(document).foundation();
+
+// SLIDER DE FONDO
+$(document).ready(function() {
+        var slider = $('#slider').leanSlider({
+            directionNav: '#slider-direction-nav',
+            controlNav: '#slider-control-nav'
+        });
+    });
